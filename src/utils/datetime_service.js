68b67db73ac45b39748e11a851516ede326c742a
Infelo <infelo453@gmail.com>
2025-10-12 15:15:11 +0545

    Changes

--- src/utils/datetime_service.js
+++ src/utils/datetime_service.js
@@ -1,16 +1,10 @@
-const moment = require('moment-timezone');
-
 class DateTimeService {
     nepalTimeDate() {
-        const now = new Date();
-        const nepalTime = new Date(now.getTime() + (5 * 60 + 45) * 60000);
-        return nepalTime;
+        return new Date();
     }
 
     getNepalDateTime(givenDate) {
-        const date = new Date(givenDate);
-        const nepalTime = new Date(date.getTime() + (5 * 60 + 45) * 60000);
-        return nepalTime;
+        return new Date(givenDate);
     }
 }
 
