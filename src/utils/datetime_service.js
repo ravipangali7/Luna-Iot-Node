eac4819fecac9d3ef23edab974cdfc21b827597e
Infelo <infelo453@gmail.com>
2025-10-12 15:20:59 +0545

    Changes

--- src/utils/datetime_service.js
+++ src/utils/datetime_service.js
@@ -1,10 +1,18 @@
 class DateTimeService {
     nepalTimeDate() {
-        return new Date();
+        const now = new Date();
+        // Convert from CST (UTC+8) to Nepal time (UTC+5:45)
+        // Nepal is 2 hours 15 minutes behind CST
+        const nepalTime = new Date(now.getTime() - (2 * 60 + 15) * 60000);
+        return nepalTime;
     }
 
     getNepalDateTime(givenDate) {
-        return new Date(givenDate);
+        const date = new Date(givenDate);
+        // Convert from CST (UTC+8) to Nepal time (UTC+5:45)
+        // Nepal is 2 hours 15 minutes behind CST
+        const nepalTime = new Date(date.getTime() - (2 * 60 + 15) * 60000);
+        return nepalTime;
     }
 }
 
